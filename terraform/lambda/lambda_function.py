200b501b95ab64329b470c6e4179b55f4c4aaadc
Charles <charles.carrere@eleve.ensai.fr>
2024-04-30 17:11:33 +0200

    fhzeio

--- terraform/lambda/lambda_function.py
+++ terraform/lambda/lambda_function.py
@@ -11,8 +11,47 @@ s3 = boto3.client("s3")
 dynamodb = boto3.resource("dynamodb")
 reckognition = boto3.client("rekognition")
 
-table = dynamodb.Table(os.getenv("table"))
+table = dynamodb.Table(os.getenv("DYNAOMO_TABLE"))
 
 
 def lambda_handler(event, context):
-    pass
+    # Pour logger
+    logger.info(json.dumps(event, indent=2))
+    # Récupération du nom du bucket
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    # Récupération du nom de l'objet
+    key = unquote_plus(event["Records"][0]["s3"]["object"]["key"])
+    # extration de l'utilisateur et de l'id de la tâche
+    user, task_id= key.split('/')[:2]
+
+    # Appel au service, en passant l'image à analyser (bucket et key)
+    # On souhaite au maximum 5 labels et uniquement les labels avec un taux de confiance > 0.75
+    # Vous pouvez faire varier ces valeurs.
+    label_data = reckognition.detect_labels(
+    Image={
+    "S3Object": {
+    "Bucket": bucket,
+    "Name": key
+    }
+    },
+    MaxLabels=5,
+    MinConfidence=0.75
+    )
+    logger.info(f"Labels data : {label_data}")
+    # On extrait les labels du résultat
+    labels = [label["Name"] for label in label_data["Labels"]]
+    logger.info(f"Labels detected : {labels}")
+
+    table.update_item(
+        Key={"user": user, "post": task_id},
+        UpdateExpression="SET labels = :labels",
+        ExpressionAttributeValues={":labels": labels},
+    )
+"""
+    logger.info(f"Labels added to table : {image_url}")
+    table.update_item(
+        Key={"user": user, "post": task_id},
+        UpdateExpression="SET image = :image",
+        ExpressionAttributeValues={":image": image_url},
+    )
+    return "Success"""
